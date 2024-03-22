8766c12a53a2e89b816feb5f6852a5902d3fed63
axiometa <dumcius99@gmail.com>
2024-03-23 00:22:16 +0200

    new file

---
+++ lib/component_library.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+class ComponentLibrary extends StatelessWidget {
+  final Function(String, Offset) onComponentSelected;
+
+  ComponentLibrary({super.key, required this.onComponentSelected});
+
+  final List<String> components = [
+    'Arduino Nano',
+    'Sensor DHT11',
+    'Sensor BMP180',
+    // Add more components as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: components.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(components[index]),
+          onTap: () =>
+              onComponentSelected(components[index], const Offset(100, 100)),
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/component_library.dart';
 
 void main() {
   runApp(const MyApp());
