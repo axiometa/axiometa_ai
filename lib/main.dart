38bf2086dafa6c9db04cbaa395016e5c4d209eba
axiometa <dumcius99@gmail.com>
2024-03-23 00:21:53 +0200

    new file 1

--- lib/main.dart
+++ lib/main.dart
@@ -281,30 +281,3 @@ class ConnectionPainter extends CustomPainter {
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
-
-class ComponentLibrary extends StatelessWidget {
-  final Function(String, Offset) onComponentSelected;
-
-  ComponentLibrary({super.key, required this.onComponentSelected});
-
-  final List<String> components = [
-    'Arduino Nano',
-    'Sensor DHT11',
-    'Sensor BMP180',
-    // Add more components as needed
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: components.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(components[index]),
-          onTap: () =>
-              onComponentSelected(components[index], const Offset(100, 100)),
-        );
-      },
-    );
-  }
-}
