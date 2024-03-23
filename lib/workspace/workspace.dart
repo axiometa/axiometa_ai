8fc7908ceb2065a180629f3fba7a41af3afcd24f
axiometa <dumcius99@gmail.com>
2024-03-24 00:06:10 +0200

    popup wires

--- lib/workspace/workspace.dart
+++ lib/workspace/workspace.dart
@@ -28,7 +28,8 @@ class workspaceState extends State<workspace> {
         ComponentData(
           name: componentName,
           position: position,
-          imagePath: 'assets/images/sensor_icons/$componentName.png', // Example path
+          imagePath:
+              'assets/images/sensor_icons/$componentName.png', // Example path
         ),
       );
     });
@@ -61,7 +62,8 @@ class workspaceState extends State<workspace> {
   }
 
   void onComponentDropped(ComponentData component, Offset globalOffset) {
-    final RenderBox renderBox = workspaceKey.currentContext!.findRenderObject()! as RenderBox;
+    final RenderBox renderBox =
+        workspaceKey.currentContext!.findRenderObject()! as RenderBox;
     final Offset localOffset = renderBox.globalToLocal(globalOffset);
     setState(() {
       component.position = localOffset;
@@ -99,45 +101,53 @@ class workspaceState extends State<workspace> {
       },
     ).then((val) {
       if (val) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Upload complete!')));
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Upload complete!')));
       }
     });
   }
 
   double distanceToSegment(Offset p1, Offset p2, Offset tapPosition) {
-  final double l2 = (p2 - p1).distanceSquared;
-  // If line length is 0, it's a point, so return distance to the point
-  if (l2 == 0) return (tapPosition - p1).distance;
+    final double l2 = (p2 - p1).distanceSquared;
+    // If line length is 0, it's a point, so return distance to the point
+    if (l2 == 0) return (tapPosition - p1).distance;
 
-  // Calculate t, the projection scalar of tapPosition on the line p1->p2
-  final double t = ((tapPosition - p1).dx * (p2 - p1).dx + (tapPosition - p1).dy * (p2 - p1).dy) / l2;
+    // Calculate t, the projection scalar of tapPosition on the line p1->p2
+    final double t = ((tapPosition - p1).dx * (p2 - p1).dx +
+            (tapPosition - p1).dy * (p2 - p1).dy) /
+        l2;
 
-  // Determine the closest point on the segment to the tapPosition
-  Offset projection;
-  if (t < 0) {
-    projection = p1; // Before p1 on the line
-  } else if (t > 1) {
-    projection = p2; // Past p2 on the line
-  } else {
-    projection = p1 + Offset((p2 - p1).dx * t, (p2 - p1).dy * t); // Projection falls on the segment
+    // Determine the closest point on the segment to the tapPosition
+    Offset projection;
+    if (t < 0) {
+      projection = p1; // Before p1 on the line
+    } else if (t > 1) {
+      projection = p2; // Past p2 on the line
+    } else {
+      projection = p1 +
+          Offset((p2 - p1).dx * t,
+              (p2 - p1).dy * t); // Projection falls on the segment
+    }
+
+    // Return the distance from tapPosition to the closest point on the segment
+    return (tapPosition - projection).distance;
   }
-
-  // Return the distance from tapPosition to the closest point on the segment
-  return (tapPosition - projection).distance;
-}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AXIOMETA', style: TextStyle(color: Colors.white, fontFamily: 'Pirulen')),
+        title: const Text('AXIOMETA',
+            style: TextStyle(color: Colors.white, fontFamily: 'Pirulen')),
         backgroundColor: Color.fromARGB(255, 5, 19, 12),
         actions: [
           if (!isProgrammingMode)
             IconButton(
               icon: Icon(isDeleteMode ? Icons.edit : Icons.delete),
               onPressed: () => setState(() => isDeleteMode = !isDeleteMode),
-              tooltip: isDeleteMode ? 'Switch to Edit Mode' : 'Switch to Delete Mode',
+              tooltip: isDeleteMode
+                  ? 'Switch to Edit Mode'
+                  : 'Switch to Delete Mode',
               color: Colors.white,
             ),
           IconButton(
@@ -148,7 +158,9 @@ class workspaceState extends State<workspace> {
                 isDeleteMode = false;
               }
             }),
-            tooltip: isProgrammingMode ? 'Exit Programming Mode' : 'Enter Programming Mode',
+            tooltip: isProgrammingMode
+                ? 'Exit Programming Mode'
+                : 'Enter Programming Mode',
             color: Colors.white,
           ),
         ],
@@ -163,7 +175,9 @@ class workspaceState extends State<workspace> {
                 children: [
                   Expanded(
                     child: SingleChildScrollView(
-                      child: Text(generatedCode, style: const TextStyle(fontFamily: 'Monospace', fontSize: 16)),
+                      child: Text(generatedCode,
+                          style: const TextStyle(
+                              fontFamily: 'Monospace', fontSize: 16)),
                     ),
                   ),
                   ElevatedButton(
@@ -171,108 +185,151 @@ class workspaceState extends State<workspace> {
                       showUploadDialog(); // Simulate upload functionality
                     },
                     child: const Text('Upload'),
-                    style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(40)),
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(40)),
                   ),
                   SizedBox(height: 10), // Adjusted spacing to 10px
                   ElevatedButton(
-                    onPressed: copyToClipboard, // Implement copy to clipboard functionality
+                    onPressed:
+                        copyToClipboard, // Implement copy to clipboard functionality
                     child: const Text('Copy to Clipboard'),
-                    style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(40)),
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(40)),
                   ),
                 ],
               ),
             ),
           ),
           Expanded(
-  flex: 2,
-  child: GestureDetector(
-    onTapUp: (TapUpDetails details) {
-      final RenderBox renderBox = workspaceKey.currentContext!.findRenderObject() as RenderBox;
-      final Offset localPosition = renderBox.globalToLocal(details.globalPosition);
-      
-      bool hitLine = false;
-      for (var component in componentsInWorkspace) {
-        for (var connection in component.connections) {
-          final Offset startPoint = component.position + Offset(100 / 2, 100 / 2);
-          final Offset endPoint = connection.position + Offset(100 / 2, 100 / 2);
-          
-          if (distanceToSegment(startPoint, endPoint, localPosition) < 5) {
-            // If a line is hit, handle the logic for line tap
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text('Connection Info'),
-                  content: Text('Tapped on wire connecting ${component.name} and ${connection.name}.'),
-                  actions: <Widget>[
-                    TextButton(
-                      child: Text('Close'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
-                );
+            flex: 2,
+            child: GestureDetector(
+              onTapUp: (TapUpDetails details) {
+                final RenderBox renderBox = workspaceKey.currentContext!
+                    .findRenderObject() as RenderBox;
+                final Offset localPosition =
+                    renderBox.globalToLocal(details.globalPosition);
+
+                bool hitLine = false;
+                for (var component in componentsInWorkspace) {
+                  for (var connection in component.connections) {
+                    final Offset startPoint =
+                        component.position + Offset(100 / 2, 100 / 2);
+                    final Offset endPoint =
+                        connection.position + Offset(100 / 2, 100 / 2);
+
+                    if (distanceToSegment(startPoint, endPoint, localPosition) < 5) {
+
+                      // Connection details for DHT11
+                      String dht11Diagram =
+                          '[DHT11]     [Arduino Nano]\n VCC    --> 5V\n Data   --> D2\n GND    --> GND\n';
+
+                      // Connection details for BMP180
+                      String bmp180Diagram =
+                          '[BMP180]    [Arduino Nano]\n VCC    --> 3.3V\n SDA    --> A4\n SCL    --> A5\n GND    --> GND\n';
+
+                      // Determine which diagram to display
+                      String diagramText = component.name == "Sensor DHT11"
+                          ? dht11Diagram
+                          : component.name == "Sensor BMP180"
+                              ? bmp180Diagram
+                              : "Unknown component";
+
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Connection Info'),
+                            content: RichText(
+                              text: TextSpan(
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16), // Default text style
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text:
+                                          'Tapped on wire connecting ${component.name} and ${connection.name}.\n\n'),
+                                  TextSpan(
+                                      text: diagramText,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                ],
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('Close'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                      hitLine = true;
+                      break; // Stop checking after a hit is found
+                    }
+                  }
+                  if (hitLine) break;
+                }
+
+                if (!hitLine) {
+                  // If no line was hit, handle the tap as a workspace tap
+                  setState(() => selectedComponent = null);
+                }
               },
-            );
-            hitLine = true;
-            break; // Stop checking after a hit is found
-          }
-        }
-        if (hitLine) break;
-      }
-      
-      if (!hitLine) {
-        // If no line was hit, handle the tap as a workspace tap
-        setState(() => selectedComponent = null);
-      }
-    },
-    child: Container(
-      key: workspaceKey,
-      color: Colors.white,
-      child: CustomPaint(
-        painter: ConnectionPainter(componentsInWorkspace),
-        child: Stack(
-          children: componentsInWorkspace.map((component) {
-            return Positioned(
-              left: component.position.dx,
-              top: component.position.dy,
-              child: Draggable<ComponentData>(
-                data: component,
-                feedback: Material(
-                  elevation: 4.0,
-                  child: Image.asset(component.imagePath, width: 100, height: 100), // Adjust size as needed
-                ),
-                onDragEnd: (details) => onComponentDropped(component, details.offset),
-                child: GestureDetector(
-                  onTap: () => onComponentTap(component),
+              child: Container(
+                key: workspaceKey,
+                color: Colors.white,
+                child: CustomPaint(
+                  painter: ConnectionPainter(componentsInWorkspace),
                   child: Stack(
-                    children: [
-                      Image.asset(component.imagePath, width: 100, height: 100), // Base Image
-                      if (selectedComponent == component) // Conditional blue overlay
-                        Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.blue.withOpacity(0.8),
+                    children: componentsInWorkspace.map((component) {
+                      return Positioned(
+                        left: component.position.dx,
+                        top: component.position.dy,
+                        child: Draggable<ComponentData>(
+                          data: component,
+                          feedback: Material(
+                            elevation: 4.0,
+                            child: Image.asset(component.imagePath,
+                                width: 100,
+                                height: 100), // Adjust size as needed
+                          ),
+                          onDragEnd: (details) =>
+                              onComponentDropped(component, details.offset),
+                          child: GestureDetector(
+                            onTap: () => onComponentTap(component),
+                            child: Stack(
+                              children: [
+                                Image.asset(component.imagePath,
+                                    width: 100, height: 100), // Base Image
+                                if (selectedComponent ==
+                                    component) // Conditional blue overlay
+                                  Container(
+                                    width: 100,
+                                    height: 100,
+                                    color: Colors.blue.withOpacity(0.8),
+                                  ),
+                              ],
+                            ),
+                          ),
                         ),
-                    ],
+                      );
+                    }).toList(),
                   ),
                 ),
               ),
-            );
-          }).toList(),
-        ),
-      ),
-    ),
-  ),
-),
-
+            ),
+          ),
           Expanded(
             flex: 1,
             child: Container(
               color: Colors.grey[100],
               child: ComponentLibrary(
-                onComponentSelected: (componentName, position) => addComponentToWorkspace(componentName, const Offset(100, 100)),
+                onComponentSelected: (componentName, position) =>
+                    addComponentToWorkspace(
+                        componentName, const Offset(100, 100)),
               ),
             ),
           ),
