13e635135f1ce5e4653e07487b140839065fc3f5
axiometa <dumcius99@gmail.com>
2024-03-25 17:02:33 +0200

    change d theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // Set the primary color
+        primaryColor: Colors.white, // Sets the background color of the App Bar
+        scaffoldBackgroundColor: Colors.white, // Sets the default background color of the Scaffold
+        // Since white is not a swatch, we do not use primarySwatch here
+        // Define other theme properties as needed
+      ),
       home: Scaffold(
         body: const Center(
           child: ModeSelector(),
